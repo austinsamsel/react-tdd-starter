5c8f7bd5c0cbeb1f7d76a15604061193b58f5725
austin samsel <austinsamsel@gmail.com>
2016-01-22 13:46:14 -0500

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var path = require('path');                // a useful node path helper library
+
+var config = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/main.js'
+  ],                // the entry point for our app
+  resolve: {
+    root: [
+      // allows us to import modules as if /src was the root.
+      // so I can do: import Comment from 'components/Comment'
+      // instead of:  import Comment from '../components/Comment' or whatever relative path would be
+      path.resolve(__dirname, './src')
+    ],
+    // allows you to require without the .js at end of filenames
+    // import Component from 'component' vs. import Component from 'component.js'
+    extensions: ['', '.js', '.json', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'), // store the bundled output in dist/bundle.js
+    filename: 'bundle.js'                  // specifying file name for our compiled assets
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: ['babel'],
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config;
