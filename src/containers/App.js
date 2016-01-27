f7c59fe7e3eb5bdb16f2f369a0db71a37de5597d
austin samsel <austinsamsel@gmail.com>
2016-01-27 18:40:59 -0500

    change welcome text

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return(
       <div style={styles}>
-        <h1 className='welcome-header'>Welcome to testing React!</h1>
+        <h1 className='welcome-header'>Welcome!</h1>
       </div>
     )
   }
